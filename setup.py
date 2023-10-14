6b822cbece8940f10dce6fbe7e74776e5565ddd1
Jong Shin <jshin@rccl.com>
2023-10-14 05:54:53 -0400

    don't install cuda conflictive packages
    Co-authored-by: Jong Shin <jshin.m@gmail.com>

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ setup(
     url="https://github.com/m-bain/whisperx",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ] + ["pyannote.audio @ git+https://github.com/pyannote/pyannote-audio@db24eb6c60a26804b1f07a6c2b39055716beb852"],
-    entry_points = {
-        'console_scripts': ['whisperx=whisperx.transcribe:cli'],
+    # install_requires=[
+    #     str(r)
+    #     for r in pkg_resources.parse_requirements(
+    #         open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    #     )
+    # ] + ["pyannote.audio @ git+https://github.com/pyannote/pyannote-audio@db24eb6c60a26804b1f07a6c2b39055716beb852"],
+    entry_points={
+        "console_scripts": ["whisperx=whisperx.transcribe:cli"],
     },
     include_package_data=True,
-    extras_require={'dev': ['pytest']},
+    extras_require={"dev": ["pytest"]},
 )
